9dc2ca57ac1166d02d580b6142123d1f8e976e7d
NicolasB <nicosm0912@gmail.com>
2023-02-06 14:27:30 -0300

    Itemlist

---
+++ src/componentes/header/ItemListContainer/ItemListContainer.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const ItemListContainer = ({greeting}) => {
+    <div className="greeting">
+        <h3 className="card-tittle">{greeting}</h3>
+    </div>
+}
+
+export default ItemListContainer
+
