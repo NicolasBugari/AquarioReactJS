55036cd593199a25b155b52250b58e4c7d224d62
NicolasB <nicosm0912@gmail.com>
2023-03-08 21:26:02 -0300

    Firebase

---
+++ src/firebase/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAwiGLAC-C14y93fBfk2WA9N2-s_kfpm8w",
+  authDomain: "proyectoreactfinalbugari.firebaseapp.com",
+  projectId: "proyectoreactfinalbugari",
+  storageBucket: "proyectoreactfinalbugari.appspot.com",
+  messagingSenderId: "340362486661",
+  appId: "1:340362486661:web:d6a8626c7ee7e3125a9aec"
+};
+
+const app = initializeApp(firebaseConfig);
